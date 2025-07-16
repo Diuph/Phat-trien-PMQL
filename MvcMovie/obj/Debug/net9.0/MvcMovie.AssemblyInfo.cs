58c75bae0eec975f1e6975541fc1d3b9f3db8fc0
Diuph <diup97613@gmail.com>
2025-07-16 17:56:21 +0700

    thuc hanh slide 67

--- MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79c84ae753fb329723726d1e5dc90ec487bce9c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+859940337936ed7d17e126f225bfe14dcd79f7a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
